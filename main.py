15794fdb2697747f4a0f58bcd06db6554d3eec75
Alex Keri <38541448+AlexK5@users.noreply.github.com>
2019-05-20 11:49:27 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+def isprime(n):
+  y=2
+  while y<=math.sqrt(n):
+    if n%y==0:
+      return(0)
+    y+=1
+  return(1)
+x=2
+total=0
+while(x<3000):
+  if isprime(x)==1:
+    total+=x
+  x+=1
+print(total)
